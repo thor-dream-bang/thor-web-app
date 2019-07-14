ba5080150e6502412ff6774dedffa68b7267607f
ady <amdersz5g7>
2019-07-14 22:40:37 +0700

    try use https for asset

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -21,7 +21,7 @@ class AppServiceProvider extends ServiceProvider
      *
      * @return void
      */
-    public function boot()
+    public function boot(UrlGenerator $url)
     {
         //
         if(env('REDIRECT_HTTPS'))
